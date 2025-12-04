f18478afa3622f100888ed93adce245714f1df31
hendrygomez <147951397+hendrygomez@users.noreply.github.com>
2025-12-04 18:34:15 -0500

    Lista de los recursos
    Array que contendra los datos de los recursos

---
+++ script.js
@@ -0,0 +1,13 @@
+const resources = [{
+    name: "NOMBRE DEL RECURSO",
+    category: "CATEGORIA DEL RECURSO",
+    description: "DESCRIPCION DEL RECURSO",
+},{
+    name: "NOMBRE DEL RECURSO",
+    category: "CATEGORIA DEL RECURSO",
+    description: "DESCRIPCION DEL RECURSO",
+},{
+    name: "NOMBRE DEL RECURSO",
+    category: "CATEGORIA DEL RECURSO",
+    description: "DESCRIPCION DEL RECURSO",
+}];
