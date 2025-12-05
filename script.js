bc5e01ba5a74656fb297385d646aba3593229a3e
isadejesusweyant <isadejesusweyant@gmail.com>
2025-12-05 13:27:44 -0500

    Changes

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const resources = [{
     name: "York County Food Bank",
     category: "Food",
-    description: "DESCRIPCION DEL RECURSO",
+    description: "DESCRIPTION OF THE RESOURCE",
 },{
     name: "PA 211",
     category: "Education",
-    description: "DESCRIPCION DEL RECURSO",
+    description: "DESCRIPTION OF THE RESOURCE",
 },{
     name: "York JCC Family Services",
     category: "Health",
-    description: "DESCRIPCION DEL RECURSO",
+    description: "DESCRIPTION OF THE RESOURCE",
 },{
     name: "Healthy Community Network",
     category: "Health",
-    description: "DESCRIPCION DEL RECURSO",
+    description: "DESCRIPTION OF THE RESOURCE",
 },{
     name: "Access York, Inc.",
     category: "Health",
-    description: "DESCRIPCION DEL RECURSO",
+    description: "DESCRIPTION OF THE RESOURCE",
 }];
 
 function displayResources(resources) {
@@ -27,7 +27,7 @@ function displayResources(resources) {
     for (let i = 0; i < resources.length; i++) {
         const r = resources[i];
         resourceList.innerHTML += `
-            <div class="resource-card">
+            <div class="resource-card-">
                 <h3>${i + 1}. ${r.name}</h3>
                 <p>Category: ${r.category}</p>
                 <p>${r.description}</p>
